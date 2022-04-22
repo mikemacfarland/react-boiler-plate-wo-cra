a55de52b0823c2b2e05d922408b803e5f2d0cc1a
mikemacfarland <m_macfarland@live.com>
2022-04-22 14:48:31 -0400

    added svg/jpg  loader to webpack

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ React boiler plate without CRA
 
 This is a react App boiler plate to develop applications without the dependency of CRA
 
+This requires work in of the following dependencies:
+
+CSS
+SVG
+file loader
+
 directed from this tutorial:
 
 https://levelup.gitconnected.com/freedom-from-create-react-app-how-to-create-react-apps-without-cra-27fadeb79c82
--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import images from './assets/images.js'
 
 const App = ()=>{
+    const imgStyle = {
+        height: 100,
+        width: 100,
+    }
+
     return(
-        <p>this is the app</p>
+        <div>
+            <p>this is the app</p>
+            <div style={imgStyle}>this is an svg<img src={images.cogLogo} alt="" /></div>
+            <div >this is a jpg<img style={imgStyle} src={images.livingRoom}/>
+            </div>
+        </div>
     )
 }
 
